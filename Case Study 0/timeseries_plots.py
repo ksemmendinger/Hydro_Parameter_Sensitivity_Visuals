8f4cd584d6c9b947e1788458daff24185863b36b
cfinkenbiner <c.finkenbiner@gmail.com>
2019-07-09 16:35:51 -0500

    start jupyter notebook

--- Case Study 0/timeseries_plots.py
+++ Case Study 0/timeseries_plots.py
@@ -9,3 +9,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 
+folder = '/Users/catiefinkenbiner/Documents/Summer Institute/CaseStudies/Hydroinformatics'
+df_obs = pd.read_csv(folder+'/results/USGS_obs.csv')
+df_model = pd.read_csv(folder+'/results/NWM_0.7.csv')
+
+plt.figure()
+plt.plot(df_obs['q_cms'],'r-',label='USGS')
+plt.plot(df_model['q_cms'],'b-',label='NWM')
+plt.legend()
