b5bf1d15b4b76de856a3b8a20c5145da06fe773f
cfinkenbiner <c.finkenbiner@gmail.com>
2019-07-18 15:26:33 -0500

    fixed errors in code

--- SWMM/approx_bayes_calc_of_defined.py
+++ SWMM/approx_bayes_calc_of_defined.py
@@ -73,11 +73,11 @@ def runABC(df_parms,df_OFs,runs,bins,color1,color2,metric):
     
     # saves models with objective functions within tolerance thresholds
     bayes_approx_nse = pd.DataFrame(results_nse,columns=None)
-    bayes_approx_nse.to_csv('output/bayes_parameters_'+metric+'.csv',index=False)
+    bayes_approx_nse.to_csv('output/bayes_parameters_NSE.csv',index=False)
     bayes_approx_pbias = pd.DataFrame(results_pbias,columns=None)
-    bayes_approx_nse.to_csv('output/bayes_parameters_'+metric+'.csv',index=False)
+    bayes_approx_pbias.to_csv('output/bayes_parameters_pbias.csv',index=False)
     bayes_approx_rmse = pd.DataFrame(results_rmse,columns=None)
-    bayes_approx_nse.to_csv('output/bayes_parameters_'+metric+'.csv',index=False)
+    bayes_approx_rmse.to_csv('output/bayes_parameters_rmse.csv',index=False)
     
     parameters = list(df_parms.columns.values)
     
