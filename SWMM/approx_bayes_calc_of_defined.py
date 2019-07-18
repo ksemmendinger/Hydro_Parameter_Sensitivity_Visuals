67a4c2bedaf379957f5c8534eb78b20d8c456495
cfinkenbiner <c.finkenbiner@gmail.com>
2019-07-18 08:58:33 -0500

    added pdfs and cdf plots

--- SWMM/approx_bayes_calc_of_defined.py
+++ SWMM/approx_bayes_calc_of_defined.py
@@ -44,7 +44,28 @@ def make_histograms(df_parms,bayes_approx,bins,alpha,cc1,cc2,parameters,metric):
         ax.set_xlabel(str(parameters[col-1]))    
     plt.legend(['Output','ABC'],fancybox=True)
     plt.tight_layout() 
-    plt.savefig(metric+'.png',dpi=300)
+    plt.savefig('output/'+metric+'.png',dpi=300)
+
+def make_cdfs_pdfs(df_parms,bayes_approx,bins,alpha,cc1,cc2,parameters,metric):
+    plt.figure(figsize=(12,12))
+    for col in np.arange(1,((df_parms.iloc[0,:]).size)):
+        plt.subplot(4,3,col)
+        ax = df_parms.iloc[:,col].plot.hist(cumulative=True, density=1,bins=bins,alpha=alpha,color=cc1)    
+        ax = bayes_approx.iloc[:,col].plot.hist(cumulative=True, density=1,bins=bins,alpha=alpha,color=cc2)
+        ax.set_xlabel(str(parameters[col]))    
+    plt.legend(['Output','ABC'],fancybox=True)
+    plt.tight_layout() 
+    plt.savefig('output/'+metric+'_cdf.png',dpi=300)
+    
+    plt.figure(figsize=(12,12))
+    for col in np.arange(1,((df_parms.iloc[0,:]).size)):
+        plt.subplot(4,3,col)
+        ax = df_parms.iloc[:,col].plot.kde(alpha=alpha,color=cc1)    
+        ax = bayes_approx.iloc[:,col].plot.kde(alpha=alpha,color=cc2)
+        ax.set_xlabel(str(parameters[col]))    
+    plt.legend(['Output','ABC'],fancybox=True)
+    plt.tight_layout() 
+    plt.savefig('output/'+metric+'_pdf.png',dpi=300)
 
 def runABC(df_parms,df_OFs,runs,bins,color1,color2,metric):
     # models with objective functions within tolerance thresholds
@@ -63,7 +84,12 @@ def runABC(df_parms,df_OFs,runs,bins,color1,color2,metric):
     # print ABC results and make figures
     print('precent of models with NSE >= to',str(tolerance_nse),'are:',str(len(results_nse)/runs),'%')
     make_histograms(df_parms,bayes_approx_nse,bins,0.5,color1,color2,parameters,'NSE')
+    make_cdfs_pdfs(df_parms,bayes_approx_nse,bins,0.5,color1,color2,parameters,'NSE')
+    
     print('precent of models with',str(-tolerance_pbias),'<= p-bias >=',str(tolerance_pbias),'are:',str(len(results_pbias)/runs),'%')
     make_histograms(df_parms,bayes_approx_pbias,bins,0.5,color1,color3,parameters,'pbias')
+    make_cdfs_pdfs(df_parms,bayes_approx_nse,bins,0.5,color1,color3,parameters,'pbias')
+    
     print('precent of models with RMSE < to',str(tolerance_rmse),'are:',str(len(results_rmse)/runs),'%')
     make_histograms(df_parms,bayes_approx_rmse,bins,0.5,color1,color4,parameters,'RMSE')
+    make_cdfs_pdfs(df_parms,bayes_approx_nse,bins,0.5,color1,color4,parameters,'RMSE')
