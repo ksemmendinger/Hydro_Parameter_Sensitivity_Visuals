f29d162b0211792bc77a45e773b9d041a66ebc33
cfinkenbiner <c.finkenbiner@gmail.com>
2019-07-23 15:36:52 -0500

    update ABC

--- TOPMODEL/approx_bayes_calc_of_defined.py
+++ TOPMODEL/approx_bayes_calc_of_defined.py
@@ -1,14 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul  8 12:57:45 2019
-
-@author: catiefinkenbiner
-"""
 import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 '''
 Approximate Baysian Calculation requires:
@@ -25,20 +19,46 @@ def approx_bayes_calc_OF(parms,OFs,simulations):
     for i in np.arange(simulations):
         # User can redefine tolerance and OF here
         if OFs.iloc[i,0] >= tolerance_nse:
-            keep_nse.append(parms.iloc[i,:])        
+            keep_nse.append(parms.iloc[i])        
     return keep_nse
 
-plt.rcParams.update({'font.size': 16})
 def make_histograms(df_parms,bayes_approx,bins,alpha,cc1,cc2,parameters,metric):
-    plt.figure(figsize=(15,9))
-    for col in np.arange(1,((df_parms.iloc[0,:]).size+1)):
-        plt.subplot(4,4,col)
-        ax = df_parms.iloc[:,col-1].plot.hist(bins=bins,alpha=alpha,color=cc1)    
-        ax = bayes_approx.iloc[:,col-1].plot.hist(bins=bins,alpha=alpha,color=cc2)
+    plt.figure(figsize=(15,12))
+    gridsize = math.ceil((np.sqrt((df_parms.iloc[0,:]).size))) # graph columns
+    
+    for col in np.arange(1,((df_parms.iloc[0,:]).size)+1):
+        plt.subplot(gridsize-1,gridsize,col)
+        ax = df_parms.iloc[:,col-1].plot.hist(bins=bins,alpha=alpha,color=cc1,linewidth=4)    
+        ax = bayes_approx.iloc[:,col-1].plot.hist(bins=bins,alpha=alpha,color=cc2,linewidth=4)
         ax.set_xlabel(str(parameters[col-1]))    
     plt.legend(['Output','ABC'],fancybox=True)
     plt.tight_layout() 
-    plt.savefig(metric+'.png',dpi=300)
+    plt.savefig('output/plots/ABC/'+metric+'_histogram.png',dpi=1000)
+
+def make_cdfs_pdfs(df_parms,bayes_approx,bins,alpha,cc1,cc2,parameters,metric):
+    plt.figure(figsize=(15,12))
+    gridsize = math.ceil((np.sqrt((df_parms.iloc[0,:]).size)))
+    
+    for col in np.arange(1,((df_parms.iloc[0,:]).size)+1):
+        plt.subplot(gridsize-1,gridsize,col)
+        ax = df_parms.iloc[:,col-1].plot.hist(cumulative=True, density=1,bins=bins,alpha=alpha,color=cc1,linewidth=4)    
+        ax = bayes_approx.iloc[:,col-1].plot.hist(cumulative=True, density=1,bins=bins,alpha=alpha,color=cc2,linewidth=4)
+        ax.set_xlabel(str(parameters[col-1]))    
+    plt.legend(['Output','ABC'],fancybox=True)
+    plt.tight_layout() 
+    plt.savefig('output/plots/ABC/'+metric+'_cdf.png',dpi=1000)
+    
+    plt.figure(figsize=(15,12))
+    gridsize = math.ceil((np.sqrt((df_parms.iloc[0,:]).size)))
+    
+    for col in np.arange(1,((df_parms.iloc[0,:]).size)+1):
+        plt.subplot(gridsize-1,gridsize,col)
+        ax = df_parms.iloc[:,col-1].plot.kde(alpha=alpha,color=cc1,linewidth=4)    
+        ax = bayes_approx.iloc[:,col-1].plot.kde(alpha=alpha,color=cc2,linewidth=4)
+        ax.set_xlabel(str(parameters[col-1]))   
+    plt.legend(['Output','ABC'],fancybox=True)
+    plt.tight_layout() 
+    plt.savefig('output/plots/ABC/'+metric+'_pdf.png',dpi=1000)
 
 def runABC(df_parms,df_OFs,runs,bins,color1,color2,metric):
     # models with objective functions within tolerance thresholds
@@ -46,9 +66,10 @@ def runABC(df_parms,df_OFs,runs,bins,color1,color2,metric):
 
     # saves models with objective functions within tolerance thresholds
     bayes_approx_nse = pd.DataFrame(results_nse,columns=None)
-    bayes_approx_nse.to_csv('bayes_parameters_'+metric+'.csv',index=False)
+    bayes_approx_nse.to_csv('output/bayes_parameters_NSE.csv',index=False)
     parameters = list(df_parms.columns.values)
     
     # print ABC results and make figures
     print('precent of models with NSE >= to',str(tolerance_nse),'are:',str(len(results_nse)/runs),'%')
-    make_histograms(df_parms,bayes_approx_nse,bins,0.5,color1,color2,parameters,metric)
+    make_histograms(df_parms,bayes_approx_nse,bins,0.5,color1,color2,parameters,'NSE')
+    make_cdfs_pdfs(df_parms,bayes_approx_nse,bins,0.5,color1,color2,parameters,'NSE')
