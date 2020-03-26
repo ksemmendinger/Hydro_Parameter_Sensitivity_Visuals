7bb80a489866f3f4b90f763bcd041c3bf2442365
cfinkenbiner <c.finkenbiner@gmail.com>
2020-03-26 11:15:54 -0400

    updated HBV-light case with new envi

--- Scripts/scatterplots.R
+++ Scripts/scatterplots.R
@@ -21,13 +21,10 @@ par_OF_scatter <- function(params, objective_fun, OF_name) {
       geom_point(size = 1, colour = "#016c59") + 
       xlab(paste0(colnames(params)[j])) +
       ylab(OF_name) + 
-<<<<<<< HEAD
       theme(axis.title = element_text(size = 25, color = "black"),
             axis.text = element_text(size = 20))
-=======
       theme(axis.title = element_text(size = "15", color = "black"),
             axis.text = element_text(size = 15))
->>>>>>> refs/remotes/origin/master
     
     # save to list for gridded plot
     plots[[paste(colnames(params)[j])]] <- ggplotGrob(sctplt)
@@ -35,11 +32,8 @@ par_OF_scatter <- function(params, objective_fun, OF_name) {
   }
   
   # save grid of parameter scatter plots for each OF
-<<<<<<< HEAD
   png(paste0("output/plots/scatter/", OF_name, ".png"), width = 20, height = 13, units = 'in', res = 1000)
-=======
   png(paste0("output/plots/scatter/", OF_name, ".png"), width = 17, height = 11, units = 'in',res = 1000)
->>>>>>> refs/remotes/origin/master
   print(ggarrange(plotlist = plots, widths = c(ceiling(sqrt(ncol(params))), ceiling(ncol(params)/ceiling(sqrt(ncol(params)))))))
   dev.off()      
   
